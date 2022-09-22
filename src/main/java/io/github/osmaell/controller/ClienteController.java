1cc6e0b552cb9deb0e7a38b1b4fa4de16898386c
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-09-22 19:27:28 -0300

    salvando um recurso

--- src/main/java/io/github/osmaell/controller/ClienteController.java
+++ src/main/java/io/github/osmaell/controller/ClienteController.java
@@ -4,10 +4,7 @@ import io.github.osmaell.domain.entity.Cliente;
 import io.github.osmaell.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -28,5 +25,11 @@ public class ClienteController {
 
         return ResponseEntity.notFound().build();
     }
-    
+
+    @PostMapping
+    public ResponseEntity<?> salvar( @RequestBody Cliente cliente ) {
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
 }
