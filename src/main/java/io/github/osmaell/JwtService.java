882f00900e482fcf2f367899650ce88ab892a6a6
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-10-08 10:10:40 -0300

    decodificando o token

--- src/main/java/io/github/osmaell/JwtService.java
+++ src/main/java/io/github/osmaell/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.osmaell;
 
 import io.github.osmaell.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,23 +20,58 @@ public class JwtService {
 
     @Value("${security.jwt.chave-assinatura")
     private String chaveAssinatura;
-    
+
     public String gerarToken(Usuario usuario) {
 
-        long expiracaoString = Long.parseLong(expiracao);
+        long expiracaoLong = Long.parseLong(expiracao);
 
         // definindo a hora que o token será expirado
-        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expiracaoString);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expiracaoLong);
 
         // convertendo um LocalDateTime para um objeto do tipo Date
         Date data = Date.from(dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant());
 
+        // retornando um token JWT
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
+    }
+
+    /**
+     * Método responsável por obter as informações
+     * do token (claims).
+     */
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token) {
+
+        try {
+            Claims claims = obterClaims(token);
+
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data = dataExpiracao
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
     }
 
 }
